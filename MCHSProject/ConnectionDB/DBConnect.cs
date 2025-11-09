2400c2936208298078664d78858c68b044c26799
bru1f0rc3 <yankeyanke793@gmail.com>
2025-11-09 11:20:00 +0300

    Add PostgreSQL connection with Dapper

---
+++ MCHSProject/ConnectionDB/DBConnect.cs
@@ -0,0 +1,21 @@
+﻿using Npgsql;
+using System.Data;
+
+namespace MCHSProject.ConnectionDB
+{
+    public class DBConnect
+    {
+        private readonly string _connectionString;
+
+        public DBConnect(IConfiguration configuration)
+        {
+            _connectionString = configuration.GetConnectionString("DefaultConnection") 
+                ?? throw new ArgumentNullException(nameof(configuration), "Connection string is not configured");
+        }
+
+        public IDbConnection CreateConnection()
+        {
+            return new NpgsqlConnection(_connectionString);
+        }
+    }
+}
