2af78fb38b9d7cc559e8f99e3bfaf9548a415aec
bru1f0rc3 <yankeyanke793@gmail.com>
2025-11-12 15:45:00 +0300

    Add User and Role models

---
+++ MCHSProject/Models/User.cs
@@ -0,0 +1,11 @@
+namespace MCHSProject.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Username { get; set; } = string.Empty;
+        public string PasswordHash { get; set; } = string.Empty;
+        public int RoleId { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
